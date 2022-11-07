50901b58903a3990f19666d574c5f5e00959bc5a
Kirk Davis <kirk5davis@gmail.com>
2022-11-06 20:56:50 -0800

    add template page for testing post

--- app.py
+++ app.py
@@ -1,50 +1,68 @@
 import os
 
 import boto3
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, render_template
 
 app = Flask(__name__)
 
 
-dynamodb_client = boto3.client('dynamodb')
-
-if os.environ.get('IS_OFFLINE'):
+if os.environ.get("IS_OFFLINE"):
     dynamodb_client = boto3.client(
-        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
+        "dynamodb",
+        region_name="localhost",
+        endpoint_url="http://localhost:8000",
     )
+else:
+    dynamodb_client = boto3.client("dynamodb")
 
 
-USERS_TABLE = os.environ['USERS_TABLE']
+USERS_TABLE = os.environ["USERS_TABLE"]
 
 
-@app.route('/users/<string:user_id>')
+@app.route("/users/<string:user_id>")
 def get_user(user_id):
     result = dynamodb_client.get_item(
-        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
+        TableName=USERS_TABLE, Key={"userId": {"S": user_id}}
     )
-    item = result.get('Item')
+    item = result.get("Item")
     if not item:
-        return jsonify({'error': 'Could not find user with provided "userId"'}), 404
+        return (
+            jsonify({"error": 'Could not find user with provided "userId"'}),
+            404,
+        )
 
     return jsonify(
-        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
+        {
+            "userId": item.get("userId").get("S"),
+            "name": item.get("name").get("S"),
+        }
     )
 
 
-@app.route('/users', methods=['POST'])
+@app.route("/users", methods=["POST"])
 def create_user():
-    user_id = request.json.get('userId')
-    name = request.json.get('name')
+    print("test json output: " + str(request.json))
+    user_id = request.json.get("userId")
+    name = request.json.get("name")
     if not user_id or not name:
-        return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
+        return (
+            jsonify({"error": 'Please provide both "userId" and "name"'}),
+            400,
+        )
 
     dynamodb_client.put_item(
-        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': name}}
+        TableName=USERS_TABLE,
+        Item={"userId": {"S": user_id}, "name": {"S": name}},
     )
 
-    return jsonify({'userId': user_id, 'name': name})
+    return jsonify({"userId": user_id, "name": name})
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+    return make_response(jsonify(error="Not found!"), 404)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
